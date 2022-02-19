fa3a1e064d8da93f320e19994c27daf9c5256c6a
swapnil patil <72620849+swapnilpatil1307@users.noreply.github.com>
2022-02-19 17:59:43 +0530

    Add files via upload

---
+++ netLabExamprojects(210940520105)/Mvc projects/Controllers/ProductsController.cs
@@ -0,0 +1,194 @@
+﻿using LabExamProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace LabExamProject.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Products> objpr = new List<Products>();
+            
+            SqlConnection connect = new SqlConnection();
+            connect.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ExamDB;Integrated Security=True;";
+            connect.Open();
+
+            SqlCommand cm = new SqlCommand();
+            cm.Connection = connect;
+            cm.CommandType = System.Data.CommandType.StoredProcedure;
+            cm.CommandText = "SelectAll";
+
+            try
+            {
+                SqlDataReader dataread = cm.ExecuteReader();
+                while(dataread.Read())
+                {
+                    Products p = new Products();
+                    p.ProductId = (int)dataread["ProductId"];
+                    p.ProductName = dataread["ProductName"].ToString();
+                    p.Rate = (decimal)dataread["Rate"];
+                    p.Description = dataread["Description"].ToString();
+                    p.CategoryName = dataread["CategoryName"].ToString();
+
+                    objpr.Add(p);
+
+                }
+
+            }
+            catch(Exception e)
+            {
+                ViewBag.msg = e;
+            }
+            finally
+            {
+
+                connect.Close();
+            }
+
+            return View(objpr);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+            
+            SqlConnection connect = new SqlConnection();
+            connect.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ExamDB;Integrated Security=True;";
+            connect.Open();
+
+            SqlCommand cm = new SqlCommand();
+            cm.Connection = connect;
+            cm.CommandType = System.Data.CommandType.StoredProcedure;
+            cm.CommandText = "fetchSingleOB";
+            cm.Parameters.AddWithValue("ProductId", id);
+
+           try
+            {
+                SqlDataReader dataread = cm.ExecuteReader();
+                Products p= null;
+                dataread.Read();
+                p = new Products();
+                {
+                    p.ProductId = (int)dataread["ProductId"];
+                    p.ProductName = dataread["ProductName"].ToString();
+                    p.Rate = (decimal)dataread["Rate"];
+                    p.Description = dataread["Description"].ToString();
+                    p.CategoryName = dataread["CategoryName"].ToString();
+                };
+            }
+            catch(Exception e)
+            {
+                ViewBag.msg = e;
+            }
+            finally
+            {
+                connect.Close();
+            }
+
+
+
+            return View();
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Products obj)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                SqlConnection connect = new SqlConnection();
+                connect.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ExamDB;Integrated Security=True;";
+                connect.Open();
+
+                SqlCommand cm = new SqlCommand();
+                cm.Connection = connect;
+                cm.CommandType = System.Data.CommandType.Text;
+                cm.CommandText = "Update Products set ProductId=@ProductId, ProductName=@ProductName,Rate=@Rate,Description=@Description,CategoryName=@CategoryName where ProductId="+id;
+            
+                cm.Parameters.AddWithValue("ProductName", obj.ProductName);
+                cm.Parameters.AddWithValue("Rate", obj.Rate);
+                cm.Parameters.AddWithValue("Description", obj.Description);
+                cm.Parameters.AddWithValue("CategoryName", obj.CategoryName);
+
+                try
+                {
+                    cm.ExecuteNonQuery();
+                    return RedirectToAction("Index");
+
+                }
+                catch(Exception e)
+                {
+                    ViewBag.msg = e;
+                }
+                finally
+                {
+                    connect.Close();
+                }
+                return View();
+
+               
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ netLabExamprojects(210940520105)/Mvc projects/Models/Products.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabExamProject.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+        
+        
+        [Display(Name = "Product Name")]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage ="plz Enter Product Name")]
+        [StringLength (50,ErrorMessage ="The {0} value cannot be greater than {1} characters")]
+        [Range(typeof(string),"2","5",ErrorMessage ="String is in between 2 and 5")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "plz Enter rate ")]
+        public decimal Rate { get; set; }
+       
+        [Required(ErrorMessage = "plz Enter discription")]
+        [StringLength(200, ErrorMessage = "The {0} value cannot be greater than {1} characters")]
+        public string Description{ get; set; }
+       
+        [Display(Name = "Category Name")]
+        [Required(ErrorMessage = "plz select categoryName")]
+        public string CategoryName { get; set; }
+
+    }
+}
